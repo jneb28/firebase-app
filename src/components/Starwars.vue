26c0f3e87366013fee2beb5f82b107f5cf2b8a86
Josh Nebeker <10528790@my.uvu.edu>
2018-10-18 22:39:39 -0600

    created vuetify list and fixed prettier errors

---
+++ src/components/Starwars.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="starwars">
+        <v-container>
+            <v-layout row wrap>
+                <v-flex v-for="i in 21" :key="people[i]" xs4>
+                    <v-card dark color="secondary" class="my-1 mx-2">
+                        <v-card-text>{{ people[i].name }}</v-card-text>
+                    </v-card>
+                </v-flex>
+            </v-layout>
+        </v-container>
+    </div>
+</template>
+
+<script>
+import { people } from "../assets/people.js";
+
+export default {
+  name: "Starwars",
+  data: function() {
+    return {
+      people
+    };
+  }
+};
+</script>
+
+<style scoped>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,20 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Starwars/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import Starwars from "@/components/Starwars.vue";
 
 export default {
   name: "home",
   components: {
-    HelloWorld
+    HelloWorld,
+    Starwars
   }
 };
 </script>
