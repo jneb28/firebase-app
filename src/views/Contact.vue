04c88eb71c629a9d25abe2edd2e7543c4a4b9925
Josh Nebeker <10528790@my.uvu.edu>
2018-10-25 00:02:27 -0600

    vuelidate form added

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -4,70 +4,47 @@
         <v-container>
             <v-layout align-center justify-center row>
                 <v-flex xs6>
-                <v-form ref="form" v-model="valid" lazy-validation>
-                    <v-text-field
-                    v-model="name"
-                    :rules="nameRules"
-                    :counter="10"
-                    label="Name"
-                    required
-                    ></v-text-field>
-                    <v-text-field
-                    v-model="email"
-                    :rules="emailRules"
-                    label="E-mail"
-                    required
-                    ></v-text-field>
+                    <form>
+                        <v-text-field
+                        v-model="name"
+                        :error-messages="nameErrors"
+                        :counter="10"
+                        label="Name"
+                        required
+                        @input="$v.name.$touch()"
+                        @blur="$v.name.$touch()"
+                        ></v-text-field>
+                        <v-text-field
+                        v-model="email"
+                        :error-messages="emailErrors"
+                        label="E-mail"
+                        required
+                        @input="$v.email.$touch()"
+                        @blur="$v.email.$touch()"
+                        ></v-text-field>
+                        <v-select
+                        v-model="select"
+                        :items="items"
+                        :error-messages="selectErrors"
+                        label="Item"
+                        required
+                        @change="$v.select.$touch()"
+                        @blur="$v.select.$touch()"
+                        ></v-select>
+                        <v-checkbox
+                        v-model="checkbox"
+                        :error-messages="checkboxErrors"
+                        label="Do you agree?"
+                        required
+                        @change="$v.checkbox.$touch()"
+                        @blur="$v.checkbox.$touch()"
+                        ></v-checkbox>
 
-                    <v-btn
-                    :disabled="!valid"
-                    @click="submit"
-                    >
-                    submit
-                    </v-btn>
-                    <v-btn @click="clear">clear</v-btn>
-                </v-form>
+                        <v-btn @click="submit">submit</v-btn>
+                        <v-btn @click="clear">clear</v-btn>
+                    </form>
                 </v-flex>
             </v-layout>
-            <form>
-                <v-text-field
-                v-model="name"
-                :error-messages="nameErrors"
-                :counter="10"
-                label="Name"
-                required
-                @input="$v.name.$touch()"
-                @blur="$v.name.$touch()"
-                ></v-text-field>
-                <v-text-field
-                v-model="email"
-                :error-messages="emailErrors"
-                label="E-mail"
-                required
-                @input="$v.email.$touch()"
-                @blur="$v.email.$touch()"
-                ></v-text-field>
-                <v-select
-                v-model="select"
-                :items="items"
-                :error-messages="selectErrors"
-                label="Item"
-                required
-                @change="$v.select.$touch()"
-                @blur="$v.select.$touch()"
-                ></v-select>
-                <v-checkbox
-                v-model="checkbox"
-                :error-messages="checkboxErrors"
-                label="Do you agree?"
-                required
-                @change="$v.checkbox.$touch()"
-                @blur="$v.checkbox.$touch()"
-                ></v-checkbox>
-
-                <v-btn @click="submit">submit</v-btn>
-                <v-btn @click="clear">clear</v-btn>
-            </form>
         </v-container>
     </div>
 </template>
